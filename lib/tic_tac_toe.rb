5bb9539325b7599c008794ab63fa6176b9f06a19
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-18 11:57:34 -0700

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,36 +1,38 @@
 require 'pry'
 
-class TicTacToe
+## REWORK
+
+class TicTacToe 
 
     WIN_COMBINATIONS = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
-        [0, 4, 8],
-        [2, 4, 6],
         [0, 3, 6],
         [1, 4, 7],
-        [2, 5, 8]
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
     ]
 
     def initialize(board = nil)
-        @board = board || Array.new(9, " ")
+        @board  = Array.new(9, " ")
     end 
 
-    def display_board
+    def display_board 
         puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
         puts "-----------"
         puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
         puts "-----------"
         puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-    end 
+    end
 
     def input_to_index(user_input)
-        user_input.to_i - 1
+        user_input = user_input.to_i - 1
     end 
 
     def move(index, token = "X")
-        @board[index] = token 
+        @board[index] = token
     end 
 
     def position_taken?(index)
@@ -41,30 +43,29 @@ class TicTacToe
         !position_taken?(index) && index.between?(0, 8)
     end 
 
-    def turn_count
-        @board.count {|turn| turn != " "}
-    end
-
-    def current_player
-        turn_count % 2 == 0 ? "X" : "O"
-    end 
-
-    def turn 
-        puts "Enter a number 1 through 9 (1 being top left, 9 being bottom right):"
+    def turn
+        puts "Please choose 1-9:"
         user_input = gets.strip
         index = input_to_index(user_input)
         if valid_move?(index)
-            token = current_player
-            move(index, token)
+            move(index, current_player)
         else 
-            turn 
-        end
+            turn
+        end 
         display_board
+    end
+
+    def turn_count 
+        @board.count {|space| space != " "}
+    end 
+
+    def current_player 
+        turn_count % 2 == 0 ? "X" : "O"
     end 
 
     def won?
         WIN_COMBINATIONS.detect do |combo|
-            position_taken?(combo[0]) && 
+            position_taken?(combo[0]) &&
             @board[combo[0]] == @board[combo[1]] &&
             @board[combo[1]] == @board[combo[2]]
         end
@@ -83,18 +84,13 @@ class TicTacToe
     end 
 
     def winner
-        won? ? @board[won?[0]] : nil 
-    end 
+        won? ? @board[won?[0]] : nil
+    end
 
-    def play 
+    def play
         until over?
-            turn 
-        end 
-
+            turn
+        end
         puts won? ? "Congratulations #{winner}!" : "Cat's Game!"
     end 
-end
-
-
-## REWORK
-
+end 
